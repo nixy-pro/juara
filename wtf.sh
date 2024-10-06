1b3ff7a4abe44c73dd737ea3125562e88e2809f9
NIXY Project <106488921+nixy-pro@users.noreply.github.com>
2024-10-06 22:18:52 +0800

    NIXY Project

--- wtf.sh
+++ wtf.sh
@@ -278,7 +278,7 @@ TEXT="
 <code>Exp Sc : </code><code>$EXPSC</code>
 <code>────────────────────</code>
 <i>Automatic Notification from Github</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"⭐ᴏʀᴅᴇʀ⭐","url":"https://t.me/sanzVPN"},{"text":"⭐ɪɴꜱᴛᴀʟʟ⭐","url":"https://wa.me/6282240074362"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"ORDER","url":"https://t.me/"},{"text":"INSTALL","url":"https://wa.me/"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
 clear
